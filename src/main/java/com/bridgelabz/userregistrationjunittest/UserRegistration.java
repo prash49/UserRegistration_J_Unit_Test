32c08112dbcb2ee53aa9798a2305619bbf595530
Prashanth <imprash4u@gmail.com>
2021-09-16 00:59:42 +0530

    Added: Welcome message

---
+++ src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userregistrationjunittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class UserRegistration
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to User Registration System" );
+    }
+}
