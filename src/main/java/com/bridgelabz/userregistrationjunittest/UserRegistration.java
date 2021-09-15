86cc4bff7443da82633c3e9963a5ac55fb406381
Prashanth <imprash4u@gmail.com>
2021-09-16 01:16:08 +0530

    Added: Validating FirstName using J unit test

--- src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
@@ -1,13 +1,17 @@
 package com.bridgelabz.userregistrationjunittest;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
- *
  */
-public class UserRegistration
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to User Registration System" );
+public class UserRegistration {
+
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
     }
 }
--- src/test/java/com/bridgelabz/userregistrationjunittest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunittest/UserRegistrationTest.java
@@ -1,13 +1,28 @@
 package com.bridgelabz.userregistrationjunittest;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class UserRegistrationTest
-{
+public class UserRegistrationTest {
+    UserRegistration userRegistration;
+    @Before
+    public void before() {
+        userRegistration = new UserRegistration();
+    }
 
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Prash");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("prash");
+        Assert.assertEquals(false, result);
+    }
 }
